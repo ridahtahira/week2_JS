3422b53ce2793189af1a48c67f0ee88c5a3452ce
ridahtahira <ridahtahira3@gmail.com>
2020-08-31 20:03:13 +0530

    Math functions

---
+++ scripts/Math.js
@@ -0,0 +1,15 @@
+var x=8;
+var y=3;
+var s=81;
+console.log("power:" + Math.pow(y,x));
+console.log("square root: "+Math.sqrt(s));
+console.log("Absolute value: "+Math.abs(y-x));
+var random=Math.random();
+console.log("Random number: "+random);
+console.log("Ceil: "+Math.ceil(random));
+console.log("Floor: "+Math.floor(random));
+console.log("Trignometric functions");
+console.log("Cosine value of 0: "+Math.cos(0 * Math.PI/180));
+console.log("Sine value of 90: "+Math.sin(90 * Math.PI/180));
+
+
