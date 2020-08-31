98775495261c638eff3c137ca6b79ae6795af673
ridahtahira <ridahtahira3@gmail.com>
2020-08-31 14:19:26 +0530

    Type-casted to string

---
+++ scripts/datatypes.js
@@ -0,0 +1,6 @@
+var numbers=[1,2,3,4,5];
+var element=numbers.indexOf(3);
+console.log(element,typeof(element));
+var cast=element.toString();
+console.log(cast,typeof(cast))
+alert("Type casted to string check console");
