060c543379dc7b9d0ff33f7e4890a0b1a041ba54
ridahtahira <ridahtahira3@gmail.com>
2020-09-01 20:27:23 +0530

    5th,7th,10th words from the HTML description

---
+++ scripts/strings.js
@@ -0,0 +1,6 @@
+var str="HTML stands for Hyper Text Markup Language"+
+"HTML is the standard markup language for creating Web pages"+
+"HTML describes the structure of a Web page";
+var arr=str.split(" ");
+console.log(arr[4],arr[6],arr[9]);
+
